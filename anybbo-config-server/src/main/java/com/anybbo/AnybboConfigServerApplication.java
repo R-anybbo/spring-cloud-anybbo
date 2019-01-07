b26244145b892d43f34a511a820529a3c496016a
anybbo <anybbo@gmail.com>
2019-01-07 22:31:46 +0800

    1. 加入Spring Cloud Config

---
+++ anybbo-config-server/src/main/java/com/anybbo/AnybboConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.anybbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class AnybboConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnybboConfigServerApplication.class, args);
+    }
+
+}
+
